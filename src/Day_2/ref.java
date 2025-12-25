5d24942c2d2140cc32b1ef44d9721b43230282ab
2405547-dev <2405547@kiit.ac.in>
2025-12-25 22:45:17 +0530

    Add files via upload

---
+++ src/Day_2/ref.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class Rectangle {
+    double length;
+    double breadth;
+    double area;
+    double perimeter;
+    Rectangle(float length, float breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+    void read() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter length: ");
+        length = scanner.nextDouble();
+        System.out.print("Enter breadth: ");
+        breadth = scanner.nextDouble();
+    }
+    void Area() {
+        area = length * breadth;
+    }
+    void Perimeter() {
+        perimeter = 2 * (length + breadth);
+    }
+    void display() {
+        System.out.println("Area of Rectangle = " + area);
+        System.out.println("Perimeter of Rectangle = " + perimeter);
+    }
+}
+public class ref{
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(0,0);
+        r.read();
+        r.Area();
+        r.Perimeter();
+        r.display();
+    }
+}
