d44ee09fc2d9b74ca90c2274204c2dcf339d9eec
2405547-dev <2405547@kiit.ac.in>
2025-12-26 11:39:39 +0530

    Update Prog_3.1.java

--- src/Day_3/Prog_3.1.java
+++ src/Day_3/Prog_3.1.java
@@ -1,1 +1,40 @@
+import java.util.Scanner;
+class TwoD {
+    double length;
+    double breadth;
+    int square = 40;
 
+    TwoD(double length,double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+    double sCost(){
+        return length*breadth*square;
+    }
+}
+class ThreeD extends TwoD{
+    double height;
+    int cubic=60;
+    ThreeD(double length,double breadth,double height){
+        super(length,breadth);
+        this.height=height;
+    }
+    double bCost(){
+        return length*breadth*height*cubic;
+    }
+}
+class Prog_3.1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter length:");
+        double length = scanner.nextFloat();
+        System.out.print("Enter breadth:");
+        double breadth = scanner.nextFloat();
+        TwoD sheet = new TwoD(length, breadth);
+        System.out.print("Enter height:");
+        double height = scanner.nextFloat();
+        ThreeD box = new ThreeD(length, breadth, height);
+        System.out.println("Cost of plastic sheet:"+ sheet.sCost());
+        System.out.println("Cost of plastic box:"+ box.bCost());
+    }
+}
