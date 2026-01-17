4e05e66b1a9808c776ae015d03ebb8b1d584d542
2405547-dev <2405547@kiit.ac.in>
2026-01-17 11:06:26 +0530

    Update Prog_1.3.java

--- src/Day_1/Prog_1.3.java
+++ src/Day_1/Prog_1.3.java
@@ -18,7 +18,7 @@ class DayNo {
         }
     }
 }
-public class Prog_1.3{
+public class Prog_1_3{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter day number:");
@@ -27,4 +27,5 @@ public class Prog_1.3{
         week1.printWeekDay();
     }
 }
+
 
